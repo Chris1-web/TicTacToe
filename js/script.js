3f900872cf000615c5aaa851fe8bc951b1882d28
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-03-27 00:48:00 +0100

    population of board with module javascript done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-const Gameboard = (function () {
-  const board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
-})();
+const documentMock = (() => ({
+  querySelector: (selector) => ({
+    innerHTML: null,
+  }),
+}))();
+
+const Gameboard = (function (doc) {
+  const boardArray = ["0", "X", "0", "X", "0", "X", "0", "X", "0"];
+  // when write to DOM is called, loop over board array and insert in DOM when it is ready
+  const writeToDOM = function () {
+    boardArray.forEach(function (board, i) {
+      if (!!doc && "querySelector" in doc) {
+        document.querySelector(`.section-${i + 1}`).textContent = board;
+      }
+    });
+  };
+  return { writeToDOM };
+})(document || documentMock);
 
 const player = function () {};
 
 const displayController = (function () {})();
+
+Gameboard.writeToDOM();
