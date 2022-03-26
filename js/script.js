daee3df2f8294babde061643ad65710078f019d6
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-03-26 23:11:03 +0100

    html, css, and js starter files ready

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const Gameboard = (function () {
+  const board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+})();
+
+const player = function () {};
+
+const displayController = (function () {})();
